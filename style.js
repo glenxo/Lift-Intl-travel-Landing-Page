937257986022d8946be1a00d379d3ae91211faaa
Glenn <glndxn80@gmail.com>
2022-12-23 16:58:16 -0500

    bringing th menu to visibility

--- style.js
+++ style.js
@@ -17,36 +17,25 @@ header.addEventListener("click", (event) => {
 });
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth < 1200) {
+  if (window.innerWidth >= 1200) {
     menu.classList.remove("fa-times");
     header.classList.remove("active");
     document.body.classList.remove("active");
   }
-});
+}); 
+
 
 (function () {
-  // select the elements you want to apply the parallax effect to
   var parallaxElements = document.querySelectorAll(".parallax");
-
-  // the function that will be called on scroll event
   function parallax() {
-    // get the current scroll position
     var scrollTop = window.pageYOffset;
-
-    // loop through all the parallax elements
     for (var i = 0; i < parallaxElements.length; i++) {
-      // get the current parallax element
       var element = parallaxElements[i];
-
-      // get the element's speed
       var speed = element.getAttribute("data-parallax-speed");
-
-      // set the element's background position using the scroll position and the speed
       element.style.backgroundPositionY = scrollTop * speed + "px";
     }
   }
 
-  // listen for the scroll event
   window.addEventListener("scroll", parallax);
 })();
 
