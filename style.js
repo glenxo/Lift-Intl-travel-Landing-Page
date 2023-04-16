c2c9f4c99228d1d341da2c399c5592fed5565aaf
glenxo <glndxn80@gmail.com>
2023-04-16 17:37:57 -0500

    adding pre loader to site

--- style.js
+++ style.js
@@ -16,6 +16,16 @@ header.addEventListener("click", (event) => {
   }
 });
 
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".preloader");
+  const content = document.querySelector(".content");
+  setTimeout(function () {
+    loader.style.display = "none";
+    content.style.display = "block";
+  }, 2000); // Change this value to control how long the pre-loader displays for
+});
+
+
 window.addEventListener("resize", () => {
   if (window.innerWidth >= 1200) {
     menu.classList.remove("fa-times");
