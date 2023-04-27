e2496d0640294acd213c176f198bad733d915069
glenxo <glndxn80@gmail.com>
2023-04-26 21:17:32 -0500

    update JS loader

--- parara puru/puru.js
+++ parara puru/puru.js
@@ -6,6 +6,15 @@ window.addEventListener("resize", () => {
   }next
 });
 
+  window.addEventListener("load", function () {
+  const loader = document.querySelector(".preloader");
+  const content = document.querySelector(".content");
+  setTimeout(function () {
+    loader.style.display = "none";
+    content.style.display = "block";
+  }, 2000); // Change this value to control how long the pre-loader displays for
+});
+
 var initialTransforms = {};
 
 // Loop through each image
--- soberania fishing eco tour/ecotour.js
+++ soberania fishing eco tour/ecotour.js
@@ -5,6 +5,15 @@ window.addEventListener("resize", () => {
       document.body.classList.remove("active");
     }next
   });
+
+    window.addEventListener("load", function () {
+  const loader = document.querySelector(".preloader");
+  const content = document.querySelector(".content");
+  setTimeout(function () {
+    loader.style.display = "none";
+    content.style.display = "block";
+  }, 2000); // Change this value to control how long the pre-loader displays for
+});
   
   const swiper = new Swiper(".swiper", {
     direction: "horizontal",
