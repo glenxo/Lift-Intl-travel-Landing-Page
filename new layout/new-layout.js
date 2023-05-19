5b784a9a93161260edcd354639b44e3d4bc689d6
glenxo <glndxn80@gmail.com>
2023-05-19 12:54:27 -0500

    adding png's

--- new layout/new-layout.js
+++ new layout/new-layout.js
@@ -1,7 +1,6 @@
 let menu = document.querySelector("#menu-btn");
 let header = document.querySelector(".header");
 
-
 menu.onclick = () => {
   console.log("Menu clicked");
   menu.classList.toggle("fa-times");
@@ -17,16 +16,6 @@ header.addEventListener("click", (event) => {
   }
 });
 
-window.addEventListener("load", function () {
-  const loader = document.querySelector(".preloader");
-  const content = document.querySelector(".content");
-  setTimeout(function () {
-    loader.style.display = "none";
-    content.style.display = "block";
-  }, 10500);
-});
-
-
 window.addEventListener("resize", () => {
   if (window.innerWidth >= 1200) {
     menu.classList.remove("fa-times");
@@ -35,6 +24,22 @@ window.addEventListener("resize", () => {
   }
 });
 
+let prevScrollPos = window.pageYOffset;
+const menuBar = document.querySelector('.bar-menu');
+
+window.addEventListener('scroll', () => {
+  const currentScrollPos = window.pageYOffset;
+  
+  if (prevScrollPos > currentScrollPos) {
+    menuBar.classList.remove('hidden');
+  } else {
+    menuBar.classList.add('hidden');
+  }
+  
+  prevScrollPos = currentScrollPos;
+});
+
+
 const links = document.querySelectorAll("a");
 
 links.forEach((link) => {
@@ -42,41 +47,20 @@ links.forEach((link) => {
     header.classList.remove("active");
   });
 });
-  
-document.addEventListener("DOMContentLoaded", function() {
-  let currentImg = null;
-  let currentLink = null;
-  const images = document.querySelectorAll("img");
-  images.forEach(function(img) {
-    img.addEventListener("click", function() {
-      if (currentImg) {
-        currentLink.style.display = "none";
-        currentImg.style.filter = "none";
-      }
-      
-      let link = document.createElement("a");
-      link.innerHTML = img.dataset.text;
-      link.href = img.dataset.link;
-      link.style.position = "absolute";
-      link.style.top = "35%";
-      link.style.left = "50%";
-      link.style.transform = "translate(-50%, -50%)";
-      link.style.textAlign = "center";
-      link.style.color = "white";
-      img.parentNode.appendChild(link);
-      
-      img.style.filter = "blur(3px)";
-      currentImg = img;
-      currentLink = link;
-    });
-  });
-  
-  window.addEventListener("scroll", function() {
-    if (currentImg) {
-      currentLink.style.display = "none";
-      currentImg.style.filter = "none";
-      currentImg = null;
-      currentLink = null;
-    }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slideshowItems = document.querySelectorAll('#slideshow li');
+  let currentItemIndex = 0;
+
+  function showNextItem() {
+    slideshowItems[currentItemIndex].classList.remove('active');
+    currentItemIndex = (currentItemIndex + 1) % slideshowItems.length;
+    slideshowItems[currentItemIndex].classList.add('active');
+  }
+
+  setInterval(showNextItem, 4000); // Change slide every 3 seconds (3000 milliseconds)
+
+  window.addEventListener("scroll", function () {
+    // No effects on destination section's image
   });
 });
