8f2d8b24375944cf23df96ae722838d0ac88481e
glenxo <glndxn80@gmail.com>
2023-07-06 19:54:40 -0500

    swiper add

--- display.js
+++ display.js
@@ -1,5 +1,4 @@
-
-const swiper = new Swiper(".swiper", {
+  const swiper = new Swiper(".swiper", {
     direction: "horizontal",
     loop: true,
     pagination: {
@@ -13,3 +12,17 @@ const swiper = new Swiper(".swiper", {
       el: ".swiper-scrollbar",
     },
   });
+  
+  (function () {
+    const parallaxElements = document.querySelectorAll(".parallax");
+    function parallax() {
+      const scrollTop = window.pageYOffset;
+      for (let i = 0; i < parallaxElements.length; i++) {
+        const element = parallaxElements[i];
+        const speed = element.getAttribute("data-parallax-speed");
+        element.style.backgroundPositionY = scrollTop * speed + "px";
+      }
+    }
+  
+    window.addEventListener("scroll", parallax);
+  })();
