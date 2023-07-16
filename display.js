dd0f3b5165c595af83c9a44a25218680169edee6
glenxo <glndxn80@gmail.com>
2023-07-16 18:22:48 -0500

    setting up the review layout

--- display.js
+++ display.js
@@ -13,19 +13,4 @@ document.addEventListener("DOMContentLoaded", function () {
       el: ".swiper-scrollbar",
     },
   });
-
-  const slideshowItems = document.querySelectorAll("#slideshow li");
-  let currentItemIndex = 0;
-
-  function showNextItem() {
-    slideshowItems[currentItemIndex].classList.remove("active");
-    currentItemIndex = (currentItemIndex + 1) % slideshowItems.length;
-    slideshowItems[currentItemIndex].classList.add("active");
-  }
-
-  setInterval(showNextItem, 4000); // Change slide every 3 seconds (3000 milliseconds)
-
-  window.addEventListener("scroll", function () {
-    // No effects on destination section's image
-  });
-});
+})
